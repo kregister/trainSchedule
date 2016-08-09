8c2d2402e7a9bbe95d51c236f5487b9bf5ce416c
Katie <katie.register@me.com>
2016-08-09 18:28:33 -0400

    heroku

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 var express = require('express'),
 	app = express(),
-	path = require('path');
-
+	path = require('path'),
+	http = require('http');
 
 var port = process.env.PORT || 3000;
 
+var server = http.createServer(app);
+
 app.get('/', function (req, res){
 	res.sendFile(path.join(__dirname +'/index.html'));
-});
+}).listen(port);
 
 
-var http = require('http').Server(app);
-
-http.listen(port, function(){
-	console.log('listening on ', port);
-});
